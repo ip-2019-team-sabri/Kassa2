74ca7af695b4e0f2fe7c9cd43e89913c874a1524
AnthonyMoortgat <44052663+AnthonyMoortgat@users.noreply.github.com>
2019-04-25 15:25:50 +0200

    Created RabbitMQ Console

---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using MessageBroker;
+using MessageBroker.Messages;
+
+namespace RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start Broker
+            Log log = Log.Instance;
+            log.Welcome();
+            log.ShowDebugMessages(true);
+
+            // Start connection
+            Connection connection = Connection.Instance;
+
+            // Create messagehandler
+            IMessageHandler messageHandler = new MessageHandler();
+
+            // Open connection with RabbitMQ server
+            connection.OpenConnection("amqKassa", "amqKassa", "10.3.56.10", "Kassa", messageHandler);
+
+            // Wait for new message
+            Console.ReadLine();
+        }
+    }
+}
